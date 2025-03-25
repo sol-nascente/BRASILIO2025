385822f479eb35c083b85aef0557d95ecd7db7fd
~Ocram~ <161485915+sol-nascente@users.noreply.github.com>
2025-03-25 08:09:40 -0300

    Update marco.py

--- marco.py
+++ marco.py
@@ -1,1 +1,13 @@
+#hashtag = comenetariio de uma linha
+'''tres aspas simples = comentario
+de
+varias
+linhas'''
 
+#variaveis
+numInt = 10
+decimal = 0.55
+string = 'palavre entre aspas'
+booleano: True or False
+
+#manipulacao de listas
