67a4c5ae27f9f2183a22db712f4e38d186affa8b
~Ocram~ <161485915+sol-nascente@users.noreply.github.com>
2025-04-10 07:52:42 -0300

    Update marco.py

--- marco.py
+++ marco.py
@@ -13,11 +13,14 @@ booleano: True or False
 #manipulacao de listas
 
 alunos = ['matheus', 'fulano'] #Lista
-print(alunos)
-alunos [1] = 'ciclano' #Substitui o item da lista com o numeros especificado
+print(alunos) #Exibe a algo, no caso, a lista
+alunos [1] = 'ciclano' #Substitui o item da lista com o numeros especificado(fulano>ciclano)
 print(alunos)
 alunos.append('beltrano') #Adiciona "beltrano" ao final
 print(alunos)
-alunos
-alunos
-alunos
+alunos.insert(2,'clarence') #Adiciona "clarence" na posicao especificada(2)
+print(alunos)
+alunos.sort() #Arruma em ordem alfabetica ou crescente
+print(alunos)
+alunos #Arruma para ordem reversa, funciona com ou sem .sort()
+print(alunos)
